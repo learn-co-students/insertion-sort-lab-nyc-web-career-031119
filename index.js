bfd207cd322bab739dada674b240e29b936fd50f
github username <github email address>
2019-07-10 15:28:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// function findMinAndRemove(array){
-//   if (array.legnth > 0){
-//     let myArray = array.slice(1)
-//     let firstValue = findMinAndRemove(myArray)
-//     let secondValue = myArray[0]
-//     if (firstValue < secondValue){
-//       return firstValue
-//     } else{
-//       return secondValue
-//     }
-//   }else{
-//     return array[0]
-//   }
-// }
-
-
-// function findMinAndRemove(array){
-//   let min = array[0]
-//   let minIndex = 0
-//   for(let i = 0; i < array.length; i++){
-//     let currentElement = array[i]
-//     if(array[i] < min){
-//       min = array[i]
-//       minIndex = i
-//     }
-//   }
-//   array.splice(minIndex, 1);
-//   return min;
-// }
-
 
 function findMinAndRemove(array){
   let minNum = array[0]
